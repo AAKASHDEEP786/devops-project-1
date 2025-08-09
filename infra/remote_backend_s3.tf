74872bd3341d8f3fcccee65d72b1d67c484c0b23
Aakash Deep <82133913+AAKASHDEEP786@users.noreply.github.com>
2025-08-09 14:56:43 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "dev-proj-1-remote-state-bucket-123456-eu-central-1"
-#     key    = "devops-project-1/terraform.tfstate"
-#     region = "eu-west-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "dev-proj-1-remote-state-bucket-123456-eu-central-1"
+    key    = "devops-project-1/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
